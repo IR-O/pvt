57a50c68b1c0188b42dd0194aaf3ba2a525aab86
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2025-08-03 01:06:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,17 +9,19 @@ API_ID = "28542531"
 # -------------------------------------------------------------
 API_HASH = "9f4889cd2437d72ede20428c07a909be"
 
-BOT_TOKEN = getenv("BOT_TOKEN", "6365132039:AAF48I0KgZe4cyHmhMiRx_K634u6BEKApDQ")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "Hey_hotties")
-BOT_USERNAME = getenv("BOT_USERNAME", "Oreo_x_music_bot")
-BOT_NAME = getenv("BOT_NAME", "σꝛᴇo ꭙ ᴍᴜꜱɪᴄ")
+BOT_TOKEN = getenv("BOT_TOKEN", "5663640542:AAG5QT3aXxyTlD7jId6okRvEpvoQ91DNqm4")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "shaeep43")
+BOT_USERNAME = getenv("BOT_USERNAME", "Iro_x_music_bot")
+BOT_NAME = getenv("BOT_NAME", "iro ꭙ ᴍᴜꜱɪᴄ")
 ASSUSERNAME = getenv("ASSUSERNAME", "PIKKU_DP_WORLD")
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://String:iro@string.bfl5lcm.mongodb.net/?retryWrites=true&w=majority&appName=String")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "MONGO_DB_URI", "mongodb+srv://pikachu:randi@cluster0.tndvlel.mongodb.net/?retryWrites=true&w=majority")
+#MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://String:iro@string.bfl5lcm.mongodb.net/?retryWrites=true&w=majority&appName=String")
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 LOGGER_ID = int(getenv("LOGGER_ID", "-1001772857132"))
 OWNER_ID = int(getenv("OWNER_ID", 6045293810))
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-HEROKU_API_KEY = getenv("HRKU-1e980a6d-beb2-4fd7-8f13-7921cef9c27b")
+HEROKU_API_KEY = getenv("HRKU-AAWXXwOoG2StptJ5_yDFO3-n-IQSrRQoikIFR24nxs3g_____wpd9s0tZzoJ")
+#HEROKU_API_KEY = getenv("HRKU-1e980a6d-beb2-4fd7-8f13-7921cef9c27b")
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/IR-O/pvt",)
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
@@ -35,7 +37,8 @@ SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "709e1a2969664491b582008
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", "5242880000"))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "5242880000"))
-STRING1 = getenv("STRING_SESSION", "BQDEFKsAgiokuBQL9u39ZqrvaCaQO3LKSggOy37pSrHZT5zVqgjq142NHplMweczk3QX9d7zyZtJRnkbAf68dI1UwZL6Zt8E-easqg2itztgxj1x9va6IZsqNUh9L6g8vo56oMeRC9zQ2B3ZIKQUAr-SQmbwKy4yaoZNV2dLrOa0CUC_F1gDWEcLdk-hdoUsQIfEeKVpVKJz8Qgj9YEd8YNgHjbt-TFE5WGcLhV12kjkCw8PCsx1sbALnoNk22TVrhc8HJQRuB5gdJxJeer8a1B4JBrSso6B84LPI0SjAHgu-xCiDI8Mi3h4YhM4c5EN-ajrSJZczpCYkp633E6eYan31cr2FgAAAAHmorCNAA")
+#STRING1 = getenv("STRING_SESSION", "BQDEFKsAgiokuBQL9u39ZqrvaCaQO3LKSggOy37pSrHZT5zVqgjq142NHplMweczk3QX9d7zyZtJRnkbAf68dI1UwZL6Zt8E-easqg2itztgxj1x9va6IZsqNUh9L6g8vo56oMeRC9zQ2B3ZIKQUAr-SQmbwKy4yaoZNV2dLrOa0CUC_F1gDWEcLdk-hdoUsQIfEeKVpVKJz8Qgj9YEd8YNgHjbt-TFE5WGcLhV12kjkCw8PCsx1sbALnoNk22TVrhc8HJQRuB5gdJxJeer8a1B4JBrSso6B84LPI0SjAHgu-xCiDI8Mi3h4YhM4c5EN-ajrSJZczpCYkp633E6eYan31cr2FgAAAAHmorCNAA")
+STRING1 = getenv("STRING_SESSION", "BQE07uIALglWIh3JmCC_u9DcOwYwQSRE5oaeMS0YS4NEfWNYCzag3RujpsM8mOF_27mluMFiZt2QqzzEjlWt3l9unGwCGExrRimbB8FY9qLy3l4yxXdzyYmFh2t3Mj6S3ZYiwrh2fopCMLJdVG4OfcSqURmL7vJU3jJaPSYQM6MDqKE7rctTLy-5qsIsEnZFrb2VXLdKi_ZfiEW2xLL8aDXLGM_xdRgDtkT1vHlFmhUjIuRz-4GCvaChFVq9v0lVzaBHL5XsxFBIaWna5m8bFf_rS4brbNqKsW0qGi_xm0cc4wwi5eMfbKK2LxaKYnHowdMsV4RjT6xNrQo9pEOXsKhDho9t7QAAAAGxC4UDAA")
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
@@ -48,7 +51,7 @@ lyrical = {}
 votemode = {}
 autoclean = []
 confirmer = {}
-START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/cfbdee8103102bcb2e5da.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/7c8a61882e4d9f5cf8f58.jpg")
 PING_IMG_URL = getenv("PING_IMG_URL", "https://telegra.ph/file/00360393a15daf7fc4e9d.jpg")
 PLAYLIST_IMG_URL = "https://telegra.ph/file/79547e01862628bb85df0.jpg"
 STATS_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
